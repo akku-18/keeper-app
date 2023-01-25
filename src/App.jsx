a96b7f400ee178f6c8fe1018ef60cad183d475b2
akku-18 <akshanshgarg18@gmail.com>
2023-01-25 22:29:20 +0530

    added custom data

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Note from "./components/note/Note";
+import notes from "./notes";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+
+      {notes.map(cards => (
+        <Note
+          key = {cards.key}
+          title = {cards.title}
+          content = {cards.content}
+        />
+      ))}
+
+      {/* <Note
+        title="Delegation"
+        content="Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+       /> */}
       <Footer />
     </div>
   );
